5b860f6565afad40a5f416700ec91c7c3d427c04
Marwa-MEZLINI <m.mezlini92@gmail.com>
2022-12-06 16:46:34 +0100

    footer styling

--- docplanner/src/components/layout/Footer.js
+++ docplanner/src/components/layout/Footer.js
@@ -1,23 +1,25 @@
+import classes from './Footer.module.scss'
+
 function Footer() {
     return (
-        <footer>
-            <p>
+        <footer className={classes.footer} >
+            <p className={classes.inner} style={{ margin: '1.6em auto' }} >
                 We are leaders in 13 countries: <a href="https://www.znanylekarz.pl/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer">Poland</a>,
-                <a href="https://www.doktortakvimi.com/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer" >Turkey</a>,
-                <a href="https://www.doctoralia.es/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer">Spain</a>,
-                <a href="https://www.miodottore.it/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer">Italy</a>,
-                <a href="https://www.jameda.de/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer">Germany</a>,
-                <a href="https://www.znamylekar.cz/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer">Czech Republic</a>,
-                <a href="https://www.doctoralia.com.pt/" target="_blank" rel="noopener noreferrer">Portugal</a>,
-                <a href="https://www.doctoralia.com.mx/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer">Mexico</a>,
-                <a href="https://www.doctoralia.co/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer">Colombia</a>,
-                <a href="http://www.doctoralia.com.br/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer">Brazil</a>,
-                <a href="http://www.doctoraliar.com/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer">Argentina</a>,
-                <a href="http://www.doctoralia.pe/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer">Peru</a> and
-                <a href="http://www.doctoralia.cl/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer">Chile</a>
+                <a href="https://www.doktortakvimi.com/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer" > Turkey</a>,
+                <a href="https://www.doctoralia.es/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer"> Spain</a>,
+                <a href="https://www.miodottore.it/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer"> Italy</a>,
+                <a href="https://www.jameda.de/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer"> Germany</a>,
+                <a href="https://www.znamylekar.cz/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer"> Czech Republic</a>,
+                <a href="https://www.doctoralia.com.pt/" target="_blank" rel="noopener noreferrer"> Portugal</a>,
+                <a href="https://www.doctoralia.com.mx/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer"> Mexico</a>,
+                <a href="https://www.doctoralia.co/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer"> Colombia</a>,
+                <a href="http://www.doctoralia.com.br/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer"> Brazil</a>,
+                <a href="http://www.doctoraliar.com/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer"> Argentina</a>,
+                <a href="http://www.doctoralia.pe/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer"> Peru</a> and
+                <a href="http://www.doctoralia.cl/?__hstc=94104962.458aec3fd7f27376578e40574e1e08c6.1669901238348.1670153884322.1670236213632.3&__hssc=94104962.1.1670236213632&__hsfp=3280789348" target="_blank" rel="noopener noreferrer"> Chile</a>
             </p>
-            <p>This site uses cookies to deliver services in accordance with this Privacy Policy. You can specify the conditions for storing or accessing cookies on your browser.</p>
-            <p>www.docplanner.com © 2022</p>
+            <p className={classes.inner} >This site uses cookies to deliver services in accordance with this Privacy Policy. You can specify the conditions for storing or accessing cookies on your browser.</p>
+            <p className={classes.inner} >www.docplanner.com © 2022</p>
         </footer>
     )
 }
