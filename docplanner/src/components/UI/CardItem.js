dc341c7598ef4092f9b209275d516e6b0d7d859e
Marwa-MEZLINI <m.mezlini92@gmail.com>
2022-12-07 12:30:55 +0100

    services section styling (+ card components)

--- docplanner/src/components/UI/CardItem.js
+++ docplanner/src/components/UI/CardItem.js
@@ -3,16 +3,18 @@ import Card from "./Card";
 
 function CardItem(props) {
     return (
-        <li className={classes.item} style={{ backgroundColor:`${props.color}`}} >
+        <li className={classes.item} style={{ backgroundColor: `${props.color}` }} >
             <Card>
-                <div className={classes.icon}>
-                    <img src={props.image} />
-                </div>
-                <div className={classes.title}>
-                    <h3>{props.title}</h3>
-                </div>
-                <div className={classes.text}>
-                    <p>{props.text}</p>
+                <div>
+                    <div className={classes.icon}>
+                        <img src={props.image} />
+                    </div>
+                    <div className={classes.title}>
+                        <h3>{props.title}</h3>
+                    </div>
+                    <div className={classes.text}>
+                        <p>{props.text}</p>
+                    </div>
                 </div>
                 <div className={classes.link}>
                     <a href='#' >{props.drop}</a>
