5dc2ab79b4e843224c597fe79c15075eec395fcd
Marwa-MEZLINI <m.mezlini92@gmail.com>
2022-12-08 16:49:19 +0100

    stats section styling

--- docplanner/src/components/sections/StatsSection.js
+++ docplanner/src/components/sections/StatsSection.js
@@ -1,14 +1,17 @@
 import StatsCardList from "../UI/statsCardList";
+import classes from './StatsSection.module.css'
 
 function StatsSection() {
     return (
-        <section>
-            <aside>
-                <h3>The world's biggest healthcare platform</h3>
-                <p>We work from 9 offices all over the world, bringing Docplanner Group to life in 13 countries.</p>
-            </aside>
-            <div>
-                <StatsCardList />
+        <section className={classes.stats} >
+            <div className={classes.inner}>
+                <aside className={classes.header} >
+                    <h3>The world's biggest healthcare platform</h3>
+                    <p>We work from 9 offices all over the world, bringing Docplanner Group to life in 13 countries.</p>
+                </aside>
+                <div className={classes.cards} >
+                    <StatsCardList />
+                </div>
             </div>
         </section>
     )
