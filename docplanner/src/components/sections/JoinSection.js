ad59776afab05457e0be5de4834ce6cedf6ec83e
Marwa-MEZLINI <m.mezlini92@gmail.com>
2022-12-09 09:22:17 +0100

    Join section styling

--- docplanner/src/components/sections/JoinSection.js
+++ docplanner/src/components/sections/JoinSection.js
@@ -1,6 +1,8 @@
+import classes from './JoinSection.module.css'
+
 function JoinSection() {
     return (
-        <section>
+        <section className={classes.join} >
             <h1>Sounds interesting? Join us now!</h1>
             <button>SEE ALL CURRENT OPENINGS</button>
         </section>
